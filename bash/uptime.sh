e6b81fd706e62b137559afb0fe1575d8c14dfc23
Thibautdlct <thibaut.delcourt@epsi.fr>
2019-05-23 14:39:43 +0200

    add ile
    script pour exporter les jours de uptime dans un fichier csv

---
+++ bash/uptime.sh
@@ -0,0 +1,10 @@
+# usage : ./up.sh /path/to/list/server
+# export csv: ./up.sh /path/to/list/server > file.csv
+# listServeur est un argument des serveurs a tester
+listServeur=$1
+echo "Serveur;uptime"
+for SERVEUR in $(cat $1)
+do
+up=$(sudo ssh $SERVEUR 'uptime' | awk '{print $3}')
+  echo "$SERVEUR;$up;"
+done
